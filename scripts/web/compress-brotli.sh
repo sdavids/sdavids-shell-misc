ae430200101a3e28b791eb674df50911fb7f25cc
Sebastian Davids <sdavids@gmx.de>
2024-01-13 16:45:10 +0100

    feat: add compress-broli script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/web/compress-brotli.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+
+#
+# Copyright (c) 2024, Sebastian Davids
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# brotli needs to be in $PATH
+# https://web.dev/uses-text-compression/
+# https://developers.google.com/speed/webp/download
+#   Mac:
+#     brew install brotli
+#   Linux:
+#     sudo apt-get install brotli
+
+set -eu
+
+if [ -z "$*" ]; then
+  echo "Usage: $0 FILE" >&2
+  exit 1
+fi
+
+if [ ! -f "$1" ]; then
+  echo "'$1' does not exist" >&2
+  exit 2
+fi
+
+# https://man.archlinux.org/man/brotli.1
+brotli "$1" -fo "$1.br"
+
+# set the creation/modification time to the original file's
+touch -c -r "$1" "$1.br"
