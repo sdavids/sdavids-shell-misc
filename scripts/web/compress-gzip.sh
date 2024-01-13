28159276cf391d2c457308b6cb9eff3aa9969b03
Sebastian Davids <sdavids@gmx.de>
2024-01-13 16:41:53 +0100

    feat: add compress-gzip script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/web/compress-gzip.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+if [ -z "$*" ]; then
+  echo "Usage: $0 FILE" >&2
+  exit 1
+fi
+
+if [ ! -f "$1" ]; then
+  echo "'$1' does not exist" >&2
+  exit 2
+fi
+
+# https://man.archlinux.org/man/gzip.1
+gzip -9fk "$1"
+
+# set the creation/modification time to the original file's
+touch -c -r "$1" "$1.gz"
