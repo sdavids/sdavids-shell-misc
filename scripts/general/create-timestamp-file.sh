53fb165374ef4a829dadf5d9d566617621ce8749
Sebastian Davids <sdavids@gmx.de>
2024-01-16 17:44:08 +0100

    feat: add create-timestamp-file script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/general/create-timestamp-file.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+#
+# Copyright (c) 2024, Sebastian Davids
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -eu
+
+if [ -z "$*" ]; then
+  echo "Usage: $0 FILE" >&2
+  exit 1
+fi
+
+readonly file="$1"
+
+printf '%s' "$(date -Iseconds -u | sed -e 's/+00:00$/Z/')" > "${file}"
