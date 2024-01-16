3cc0bde4eaa83a1a59bec6d6a336a3b14fd1c378
Sebastian Davids <sdavids@gmx.de>
2024-01-16 19:31:29 +0100

    feat: add minify-gif script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/web/minify-gif.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+#
+# Copyright (c) 2024, Sebastian Davids
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# gifsicle needs to be in $PATH
+#   Mac:
+#     brew install gifsicle
+#   Linux:
+#     sudo apt-get install gifsicle
+
+set -eu
+
+readonly base_dir="${1:-$PWD}"
+
+# https://man.archlinux.org/man/gifsicle.1
+find "${base_dir}" \
+  -type f \
+  -name '*.gif' \
+  -exec sh -c 'mv "$1" "$1.tmp"; gifsicle --optimize=3 --lossy=80 --colors 256 "$1.tmp" -o "$1"; rm "$1.tmp"' shell {} \;
