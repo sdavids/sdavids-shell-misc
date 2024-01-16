49ae5e389f9806aa2ae73fd7b088fe913f90a630
Sebastian Davids <sdavids@gmx.de>
2024-01-16 17:15:54 +0100

    feat: add minify-json-tags script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/web/minify-json-tags.mjs
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2024, Sebastian Davids
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Prerequisite:
+//   npm i --save-dev domutils dom-serializer htmlparser2
+
+import * as htmlparser2 from 'htmlparser2';
+import { findAll, findOne, replaceElement, textContent } from 'domutils';
+import { access, readFile, writeFile } from 'fs/promises';
+import { relative } from 'node:path';
+import { cwd } from 'node:process';
+import { render } from 'dom-serializer';
+
+/**
+ * @param {ChildNode} node
+ * @return {boolean}
+ */
+const isElement = (node) => node.nodeType === 1;
+
+/**
+ * @param {Element} elem
+ * @return {boolean}
+ */
+const findJsonStructuredScriptTags = (elem) => {
+  if (elem.name !== 'script') {
+    return false;
+  }
+  const { type } = elem.attribs;
+  if (type === 'importmap') {
+    return true;
+    // eslint-disable-next-line require-unicode-regexp
+  } else if (/^application\/(?:.+\+)?json$/.test(type)) {
+    return true;
+  }
+  return false;
+};
+
+if (process.argv.length < 3) {
+  // eslint-disable-next-line no-console
+  console.error(`Usage: ${relative(cwd(), import.meta.filename)} FILE`);
+  process.exit(1);
+}
+
+const file = process.argv[2];
+
+try {
+  await access(file);
+} catch {
+  // eslint-disable-next-line no-console
+  console.error(`${file} does not exist`);
+  process.exit(2);
+}
+
+const html = await readFile(file, 'utf8');
+
+const dom = htmlparser2.parseDocument(html);
+
+const jsonTags = findAll(
+  findJsonStructuredScriptTags,
+  dom.children.filter(isElement),
+);
+
+if (jsonTags.length === 0) {
+  process.exit();
+}
+
+for (const jsonTag of jsonTags) {
+  const text = textContent(jsonTag).trim();
+  if (text === '') {
+    continue;
+  }
+  const minified = JSON.stringify(JSON.parse(text));
+  const { type } = jsonTag.attribs;
+  const newDom = htmlparser2.parseDocument(
+    `<script type="${type}">${minified}</script>`,
+  );
+  const changed = findOne(findJsonStructuredScriptTags, newDom.children);
+  replaceElement(jsonTag, changed);
+}
+
+const updatedHtml = render(dom);
+
+await writeFile(file, updatedHtml);
