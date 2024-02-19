935dcbfd27010c38baef167549b6bd73a37a48ca
Sebastian Davids <sdavids@gmx.de>
2024-02-19 17:47:20 +0100

    feat: add create-self-signed-cert script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/cert/go/go.mod
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+// SPDX-License-Identifier: Apache-2.0
+
+// https://go.dev/doc/modules/gomod-ref
+
+module sdavids.de/sdavids-shell-misc-cert
+
+go 1.22
+
+toolchain go1.22.4
---
+++ scripts/cert/spring-boot/settings.gradle.kts
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+// SPDX-License-Identifier: Apache-2.0
+
+// https://docs.gradle.org/current/dsl/org.gradle.api.initialization.Settings.html
+
+rootProject.name = "sdavids-shell-misc-cert-spring-boot"
+
+dependencyResolutionManagement {
+  repositories {
+    mavenCentral()
+  }
+}
+
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
