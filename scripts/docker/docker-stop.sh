4fb8a721f868db990df9c45aacd67663c9d106fc
Sebastian Davids <sdavids@gmx.de>
2024-02-21 10:38:26 +0100

    feat: add docker-stop script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/docker/docker-stop.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+readonly container_name='sdavids-shell-misc-docker-example'
+
+container_id="$(docker container ls --all --quiet --filter="name=^/${container_name}$")"
+
+if [ -n "${container_id}" ]; then
+  docker stop "${container_id}" > /dev/null
+fi
+
+readonly network_name='sdavids-shell-misc'
+
+if docker network inspect "${network_name}" > /dev/null 2>&1 ; then
+    docker network rm "${network_name}" > /dev/null
+fi
