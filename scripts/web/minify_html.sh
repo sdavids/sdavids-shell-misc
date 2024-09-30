ff8eb9be8f1d8d90cebbdc039bed03ee89753457
Sebastian Davids <sdavids@gmx.de>
2024-09-30 19:05:25 +0200

    feat: add minify_html script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/web/minify_html.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+# npx needs to be in $PATH
+
+set -eu
+
+readonly base_dir="${1:-$PWD}"
+
+# https://www.npmjs.com/package/html-minifier-terser#options-quick-reference
+find "${base_dir}" -type f -name '*.html' -exec \
+  npx --yes --quiet html-minifier-terser {} \
+  --collapse-boolean-attributes \
+  --collapse-whitespace \
+  --collapse-inline-tag-whitespace \
+  --decode-entities \
+  --minify-css \
+  --quote-character \" \
+  --remove-comments \
+  --remove-empty-attributes \
+  --remove-redundant-attributes \
+  --remove-script-type-attributes \
+  --remove-style-link-type-attributes \
+  --sort-attributes \
+  --sort-class-name \
+  --use-short-doctype \
+  -o "{}.tmp" \;
+
+# rename *.html.tmp to *.html
+find "${base_dir}" \
+  -type f \
+  -name '*.html.tmp' \
+  -exec sh -c 'f="$1"; mv -- "$f" "${f%.html.tmp}.html"' shell {} \;
