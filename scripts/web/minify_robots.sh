ae43fe57b7186658f156c54f4e2a908db7c19df3
Sebastian Davids <sdavids@gmx.de>
2024-09-30 19:04:00 +0200

    feat: add minify_robots script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/web/minify_robots.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+readonly base_dir="${1:-$PWD}"
+
+readonly file="${base_dir}/robots.txt"
+
+if [ "$(uname)" = 'Darwin' ]; then
+  # commented lines
+  sed -i '' '/^[@#]/d' "${file}"
+  # empty lines
+  sed -i '' '/^[[:space:]]*$/d' "${file}"
+else
+  # commented lines
+  sed -i '/^[@#]/d' "${file}"
+  # empty lines
+  sed -i '/^[[:space:]]*$/d' "${file}"
+fi
