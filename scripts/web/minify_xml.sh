05259f890bd2c85bee02b0ac95ae5c05d41c999b
Sebastian Davids <sdavids@gmx.de>
2024-09-30 19:03:12 +0200

    feat: add minify_xml script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/web/minify_xml.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+# npx needs to be in $PATH
+
+set -eu
+
+readonly base_dir="${1:-$PWD}"
+
+# https://www.npmjs.com/package/minify-xml#options
+find "${base_dir}" -type f -name '*.xml' -exec \
+  npx --yes --quiet minify-xml {} \
+  --remove-schema-location-attributes \
+  --in-place \;
