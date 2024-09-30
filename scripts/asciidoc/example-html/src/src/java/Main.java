15367397e42775faed8772eb4d7c0a232b153c6a
Sebastian Davids <sdavids@gmx.de>
2024-09-30 18:16:16 +0200

    feat: add asciidoc_html_build script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/asciidoc/example-html/src/src/java/Main.java
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+// SPDX-License-Identifier: Apache-2.0
+
+class Main {
+  // tag::main[] <1>
+  void main() {
+    System.out.println("Hello, World!");
+  }
+  // end::main[] <2>
+}
