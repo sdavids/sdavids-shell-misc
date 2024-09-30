003920b213c9b4e2ee2be79b5eb87465ad40c13f
Sebastian Davids <sdavids@gmx.de>
2024-09-30 18:31:32 +0200

    feat: add docker_start script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/docker/docker_start.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+readonly http_port="${1:-3000}"
+
+readonly tag='local'
+
+# https://docs.docker.com/reference/cli/docker/image/tag/#description
+readonly namespace='de.sdavids'
+readonly repository='sdavids-shell-misc'
+
+readonly label_group='de.sdavids.docker.group'
+
+readonly label="${label_group}=${repository}"
+
+readonly image_name="${namespace}/${repository}"
+
+readonly container_name='sdavids-shell-misc-docker-example'
+
+readonly host_name='localhost'
+
+readonly network_name="${repository}"
+
+docker network inspect "${network_name}" >/dev/null 2>&1 \
+  || docker network create \
+    --driver bridge "${network_name}" \
+    --label "${label_group}=${namespace}" >/dev/null
+
+# to ensure ${label} is set, we use --label "${label}"
+# which might overwrite the label ${label_group} of the image
+docker container run \
+  --init \
+  --rm \
+  --detach \
+  --user www-data \
+  --read-only \
+  --security-opt='no-new-privileges=true' \
+  --cap-drop=all \
+  --network="${network_name}" \
+  --publish "${http_port}:3000" \
+  --name "${container_name}" \
+  --label "${label}" \
+  "${image_name}:${tag}" >/dev/null
+
+readonly url="http://${host_name}:${http_port}"
+
+printf '\nListen local: %s\n' "${url}"
+
+if command -v pbcopy >/dev/null 2>&1; then
+  printf '%s' "${url}" | pbcopy
+  printf '\nThe URL has been copied to the clipboard.\n'
+elif command -v xclip >/dev/null 2>&1; then
+  printf '%s' "${url}" | xclip -selection clipboard
+  printf '\nThe URL has been copied to the clipboard.\n'
+elif command -v wl-copy >/dev/null 2>&1; then
+  printf '%s' "${url}" | wl-copy
+  printf '\nThe URL has been copied to the clipboard.\n'
+fi
