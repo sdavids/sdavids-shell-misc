2d823df9d6aad6ff8cf3616bc50c0d3904b2fa46
Sebastian Davids <sdavids@gmx.de>
2024-09-30 18:28:58 +0200

    feat: add docker_inspect script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/docker/docker_inspect.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+readonly container_name='sdavids-shell-misc-docker-example'
+
+docker container inspect \
+  "${container_name}"
