f38f583062208a51d3016df9030cd6c0f9339c4b
Sebastian Davids <sdavids@gmx.de>
2024-09-30 19:15:08 +0200

    feat: add git_is_working_tree_clean script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/git/git_is_working_tree_clean.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+readonly base_dir="${1:-$PWD}"
+
+(
+  cd "${base_dir}"
+
+  if [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" != 'true' ]; then
+    echo "'${base_dir}' is not a git repository" >&2
+    exit 1
+  fi
+
+  test -z "$(git status --porcelain=v1 2>/dev/null)"
+)
