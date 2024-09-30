4287e74bd8ab410b97154c3b8b8281c3d74f39c1
Sebastian Davids <sdavids@gmx.de>
2024-09-30 18:29:38 +0200

    feat: add docker_sh script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/docker/docker_sh.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+readonly container_name='sdavids-shell-misc-docker-example'
+
+docker container exec \
+  --interactive \
+  --tty \
+  "${container_name}" \
+  sh
