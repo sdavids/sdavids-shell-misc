1451218e5fd46ea3b6b2d5d171202a4310c6d5c2
Sebastian Davids <sdavids@gmx.de>
2024-09-30 19:00:59 +0200

    feat: add minify_webmanifest script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/web/minify_webmanifest.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+# jq needs to be in $PATH
+#   Mac:
+#     brew install jq
+#   Linux:
+#     sudo apt-get install jq
+
+set -eu
+
+if [ -z "$*" ]; then
+  echo "Usage: $0 FILE" >&2
+  exit 1
+fi
+
+readonly file="$1"
+
+if [ ! -f "${file}" ]; then
+  echo "'${file}' does not exist" >&2
+  exit 2
+fi
+
+readonly tmp_file="${file}.tmp"
+
+mv "${file}" "${tmp_file}"
+jq -c . "${tmp_file}" >"${file}"
+rm "${tmp_file}"
