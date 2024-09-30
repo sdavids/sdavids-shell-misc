333a79070d445db50b9eadb21b00edabb3d2ce04
Sebastian Davids <sdavids@gmx.de>
2024-09-30 19:21:08 +0200

    feat: add create_timestamp_file script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/general/create_timestamp_file.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+if [ -z "$*" ]; then
+  echo "Usage: $0 FILE" >&2
+  exit 1
+fi
+
+readonly file="$1"
+
+printf '%s' "$(date -Iseconds -u | sed -e 's/+00:00$/Z/')" >"${file}"
