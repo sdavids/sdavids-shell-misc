e02d6a6d1ec14b4d176b54dc518b7774fb561f19
Sebastian Davids <sdavids@gmx.de>
2024-09-30 18:57:39 +0200

    feat: add minify_png script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/web/minify_png.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+# optipng
+#   Mac:
+#     brew install optipng
+#   Linux:
+#     sudo apt-get install optipng
+
+# oxipng
+#   Mac:
+#     brew install optipng oxipng
+#   Linux:
+#     sudo apt-get install optipng
+#     https://github.com/shssoichiro/oxipng/issues/69
+
+set -eu
+
+readonly base_dir="${1:-$PWD}"
+
+# https://man.archlinux.org/man/optipng.1
+if command -v optipng 1>/dev/null; then
+  find "${base_dir}" \
+    -type f \
+    -name '*.png' \
+    -exec optipng -quiet -o5 -preserve {} \;
+fi
+
+# https://github.com/shssoichiro/oxipng
+if command -v oxipng 1>/dev/null; then
+  oxipng --quiet --opt 4 --strip all --alpha --preserve --recursive "${base_dir}"
+fi
