82245d41f8c57a62c5abb5a9ee2b85f249f9f6a7
Sebastian Davids <sdavids@gmx.de>
2024-09-30 19:12:22 +0200

    feat: add git_author_date_last script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/git/git_author_date_last.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2024 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+readonly base_dir="${1:-$PWD}"
+
+readonly file="${2:-}"
+
+(
+  cd "${base_dir}"
+
+  if [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" != 'true' ]; then
+    echo "'${base_dir}' is not a git repository" >&2
+    exit 1
+  fi
+
+  if [ -n "${file}" ]; then
+    printf '%s %s\n' "$(git --no-pager log --format=%aI -- "${file}" | head -n 1)" "${file}"
+  else
+    git ls-files | while IFS= read -r f; do
+      printf '%s %s\n' "$(git --no-pager log --format=%aI -- "${f}" | head -n 1)" "${f}"
+    done
+  fi
+)
