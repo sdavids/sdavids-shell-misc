abe529c0099a882e815f1c6b06beee07e3cdc4e7
Sebastian Davids <sdavids@gmx.de>
2025-02-27 07:59:26 +0100

    choe: update Gradle to 8.13
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- scripts/cert/java/quarkus/settings.gradle.kts
+++ scripts/cert/java/quarkus/settings.gradle.kts
@@ -11,5 +11,9 @@ dependencyResolutionManagement {
   }
 }
 
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention").version("0.9.0")
+}
+
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
