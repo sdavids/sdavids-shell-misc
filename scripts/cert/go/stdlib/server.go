52b21b51a17cabce1a53a3b5182853008862657c
Sebastian Davids <sdavids@gmx.de>
2025-03-03 19:24:41 +0100

    feat: add TImeoutHandler
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- scripts/cert/go/stdlib/server.go
+++ scripts/cert/go/stdlib/server.go
@@ -11,20 +11,27 @@ import (
 	"time"
 )
 
-func main() {
-	const port = 3000
+const port = 3000
 
+const (
+	readTimeout    = 5 * time.Second
+	writeTimeout   = 5 * time.Second
+	idleTimeout    = 5 * time.Second
+	handlerTimeout = 2 * time.Second
+)
+
+func main() {
 	server := http.Server{
 		Addr:         fmt.Sprintf(":%d", port),
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 5 * time.Second,
-		IdleTimeout:  5 * time.Second,
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+		IdleTimeout:  idleTimeout,
+		Handler: http.TimeoutHandler(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
 			_, err := w.Write([]byte("<!doctype html><title>Test</title><h1>Test</h1>"))
 			if err != nil {
 				slog.Error("handle response", slog.Any("error", err))
 			}
-		}),
+		}), handlerTimeout, ""),
 	}
 	defer func(server *http.Server) {
 		if err := server.Close(); err != nil {
