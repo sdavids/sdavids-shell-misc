29ae6a49cc0395225d44d8d14b936941977d6b53
Sebastian Davids <sdavids@gmx.de>
2025-03-12 14:13:36 +0100

    feat: improve nodejs server
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- scripts/cert/js/nodejs/server.mjs
+++ scripts/cert/js/nodejs/server.mjs
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: © 2020 Sebastian Davids <sdavids@gmx.de>
 // SPDX-License-Identifier: Apache-2.0
 
+/* eslint-disable no-console */
+
 import { readFileSync } from 'node:fs';
 import process from 'node:process';
 
@@ -10,31 +12,75 @@ import process from 'node:process';
     process.exit(70); // EX_SOFTWARE
   }),
 );
-['SIGINT', 'SIGTERM'].forEach((s) => process.once(s, () => process.exit(0)));
+['SIGINT', 'SIGTERM'].forEach((signal) =>
+  process.on(signal, () => process.exit(0)),
+);
+
+// eslint-disable-next-line dot-notation
+const port = Number(process.env['PORT'] ?? 3000);
+if (isNaN(port) || port < 1 || port > 65535) {
+  console.error(`Illegal port '${port}' [1..65535]`);
+  process.exit(64); // EX_USAGE
+}
+// eslint-disable-next-line dot-notation
+const keyPath = process.env['KEY_PATH'] ?? 'key.pem';
+// eslint-disable-next-line dot-notation
+const certPath = process.env['CERT_PATH'] ?? 'cert.pem';
 
 // eslint-disable-next-line init-declarations
-let https;
+let engine;
 try {
-  https = await import('node:https');
+  engine = await import('node:https');
 } catch {
   console.error('https support is disabled');
   process.exit(78); // EX_CONFIG
 }
 
-const port = 3000;
+const options = {};
+try {
+  options.cert = readFileSync(certPath);
+} catch {
+  console.error(`certificate '${certPath}' not found`);
+  process.exit(64); // EX_USAGE
+}
+try {
+  options.key = readFileSync(keyPath);
+} catch {
+  console.error(`key '${keyPath}' not found`);
+  process.exit(64); // EX_USAGE
+}
 
-const server = https.createServer(
-  {
-    key: readFileSync('key.pem'),
-    cert: readFileSync('cert.pem'),
-  },
-  (_, w) => {
-    w.writeHead(200).end('<!doctype html><title>Test</title><h1>Test</h1>');
-  },
-);
+const server = engine.createServer(options, (_, w) => {
+  w.writeHead(200).end('<!doctype html><title>Test</title><h1>Test</h1>');
+});
+
 server.keepAliveTimeout = 5000;
-server.requestTimeout = 5000;
-server.timeout = 5000;
-server.listen(port);
+server.requestTimeout = 10000;
+server.timeout = 15000;
 
-console.log(`Listen local: https://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`Listen local: https://localhost:${port}`);
+});
+
+server.on('timeout', (socket) => {
+  socket.destroy();
+});
+
+server.on('error', (err) => {
+  if (err.syscall === 'listen') {
+    switch (err.code) {
+      case 'EACCES':
+        console.error('Port requires elevated privileges');
+        process.exit(77); // EX_NOPERM
+        break;
+      case 'EADDRINUSE':
+        console.error('Port is already in use');
+        process.exit(75); // EX_TEMPFAIL
+        break;
+      default:
+      // just log
+    }
+  }
+
+  console.error(err);
+});
