e933db55f502625801969267a5cf661b94cfb5ca
Sebastian Davids <sdavids@gmx.de>
2025-03-26 18:25:27 +0100

    feat: add xcode_simulator_cleanup script
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ scripts/swift/xcode_simulator_cleanup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# SPDX-FileCopyrightText: © 2025 Sebastian Davids <sdavids@gmx.de>
+# SPDX-License-Identifier: Apache-2.0
+
+set -eu
+
+xcrun simctl delete unavailable
+xcrun simctl erase all
