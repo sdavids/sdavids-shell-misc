ba0b3ae92565bd8798af805470069bf423e31bc3
Sebastian Davids <sdavids@gmx.de>
2025-05-03 12:21:46 +0200

    refactor: verify_self_signed_cert
    - do not use positional params
    - better error messages
    - improved docs
    
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- scripts/cert/verify_self_signed_cert.sh
+++ scripts/cert/verify_self_signed_cert.sh
@@ -9,34 +9,51 @@
 
 set -Eeu -o pipefail -o posix
 
-readonly base_dir="${1:-$PWD}"
+while getopts ':d:' opt; do
+  case "${opt}" in
+    d)
+      base_dir="${OPTARG}"
+      ;;
+    ?)
+      echo "Usage: $0 [-d <dir>]" >&2
+      exit 1
+      ;;
+  esac
+done
+
+readonly base_dir="${base_dir:-$PWD}"
 
 readonly cert_path="${base_dir}/cert.pem"
 readonly key_path="${base_dir}/key.pem"
 
+if [ ! -e "${cert_path}" ]; then
+  printf "The certificate '%s' does not exist.\n" "${cert_path}" >&2
+  exit 1
+fi
+
 if [ "$(uname)" = 'Darwin' ]; then
   if [ "$(stat -f '%A' "${cert_path}")" != '600' ]; then
     printf '\nWARNING: cert.pem does not have the correct permissions. You can change them via:\n\n\tchmod 600 %s\n' "${cert_path}"
-    exit 1
+    exit 2
   fi
 
   if [ "$(stat -f '%A' "${key_path}")" != '600' ]; then
     printf '\nWARNING: key.pem does not have the correct permissions. You can change them via:\n\n\tchmod 600 %s\n' "${key_path}"
-    exit 2
+    exit 3
   fi
 else
   if [ "$(stat -c '%a' "${cert_path}")" != '600' ]; then
     printf '\nWARNING: cert.pem does not have the correct permissions. You can change them via:\n\n\tchmod 600 %s\n' "${cert_path}"
-    exit 3
+    exit 4
   fi
 
   if [ "$(stat -c '%a' "${key_path}")" != '600' ]; then
     printf '\nWARNING: key.pem does not have the correct permissions. You can change them via:\n\n\tchmod 600 %s\n' "${key_path}"
-    exit 4
+    exit 5
   fi
 fi
 
-host_name="$(openssl x509 -ext subjectAltName -noout -in cert.pem | grep 'DNS:' | sed 's/DNS:\(.*\)/\1/' | awk '{$1=$1};1')"
+host_name="$(openssl x509 -ext subjectAltName -noout -in "${cert_path}" | grep 'DNS:' | sed 's/DNS:\(.*\)/\1/' | awk '{$1=$1};1')"
 readonly host_name
 
 if [ "$(uname)" = 'Darwin' ]; then
