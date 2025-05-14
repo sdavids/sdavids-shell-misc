59a3bb7adeaa49cefcb76adcb8c56da129f1699f
Sebastian Davids <sdavids@gmx.de>
2025-05-14 15:52:29 +0200

    also remove docker network
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- scripts/docker/docker_remove.sh
+++ scripts/docker/docker_remove.sh
@@ -8,10 +8,16 @@ set -eu
 readonly container_name='sdavids-shell-misc-docker-example'
 
 if [ -n "$(docker container ls --all --quiet --filter="name=^/${container_name}$")" ]; then
-  docker container stop "${container_name}"
+  docker container stop "${container_name}" >/dev/null
 fi
 
 # container not started with --rm ?
 if [ -n "$(docker container ls --all --quiet --filter="name=^/${container_name}$")" ]; then
-  docker container remove --force --volumes "${container_name}"
+  docker container remove --force --volumes "${container_name}" >/dev/null
+fi
+
+readonly network_name='sdavids-shell-misc'
+
+if docker network inspect "${network_name}" >/dev/null 2>&1; then
+  docker network rm "${network_name}" >/dev/null
 fi
