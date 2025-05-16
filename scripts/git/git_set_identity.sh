655b8bc392cee86d4d884e5fc4849a76fa6f776c
Sebastian Davids <sdavids@gmx.de>
2025-05-16 16:42:42 +0200

    add -a and -r to git_set_identity
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- scripts/git/git_set_identity.sh
+++ scripts/git/git_set_identity.sh
@@ -27,8 +27,11 @@ fi
 
 set -e
 
-while getopts ':d:e:n:s:' opt; do
+while getopts ':a:d:e:n:rs:' opt; do
   case "${opt}" in
+    a)
+      allowed_signers_file="${OPTARG}"
+      ;;
     d)
       base_dir="${OPTARG}"
       ;;
@@ -38,11 +41,14 @@ while getopts ':d:e:n:s:' opt; do
     n)
       name="${OPTARG}"
       ;;
+    r)
+      recurse='true'
+      ;;
     s)
       ssh_key_path="${OPTARG}"
       ;;
     ?)
-      echo "Usage: $0 [-d <base_dir>] [-e <email>] [-n <name>] [-s <ssh_key_path>]" >&2
+      echo "Usage: $0 [-a <allowed_signers_file>] [-d <base_dir>] [-e <email>] [-n <name>] [-r] [-s <ssh_key_path>]" >&2
       exit 1
       ;;
   esac
@@ -51,6 +57,7 @@ done
 readonly base_dir="${base_dir:-$PWD}"
 readonly email
 readonly name
+readonly recurse="${recurse:-false}"
 readonly ssh_key_path="${ssh_key_path:-}"
 
 if [ ! -d "${base_dir}" ]; then
@@ -85,12 +92,19 @@ if [ -n "${ssh_key_path}" ]; then
     if [ "${is_ssh_key}" = 0 ]; then
       is_ssh_key='true'
       key="${ssh_key_path}"
+
+      if [ -n "${allowed_signers_file+x}" ] && [ ! -f "${allowed_signers_file}" ]; then
+        printf "The allowed signers file '%s' does not exist--will not set gpg.ssh.allowedSignersFile\n" "${allowed_signers_file}" >&2
+        allowed_signers_file=''
+      fi
     else
       key=''
+      allowed_signers_file=''
       printf "file '%s' is not valid SSH key--disabling SSH signing.\n\n" "${ssh_key_path}" >&2
     fi
   else
     key=''
+    allowed_signers_file=''
     printf "file '%s' does not exists--disabling SSH signing.\n\n" "${ssh_key_path}" >&2
   fi
 else
@@ -111,54 +125,79 @@ else
       printf "gpg.program '%s' does not exists--disabling GPG signing.\n\n" "${gpg_program}" >&2
     fi
   fi
+  allowed_signers_file=''
 fi
 
 unset gpg_program
 readonly key
+readonly allowed_signers_file
 readonly is_ssh_key
+
+set_identity_no_sign() {
+  if [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = 'true' ]; then
+    # https://git-scm.com/docs/git-config#_variables
+    git config set user.name "${name}"
+    git config set user.email "${email}"
+    git config unset user.signingkey
+    git config set --bool commit.gpgsign false
+    git config set --bool tag.gpgsign false
+    git config set --bool tag.forcesignannotated false
+    git config set gpg.format openpgp
+    git config unset gpg.ssh.allowedSignersFile
+    echo "$(git config get user.name) <$(git config get user.email)> - $PWD"
+  fi
+}
+
+set_identity_with_sign() {
+  if [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = 'true' ]; then
+    # https://git-scm.com/docs/git-config#_variables
+    git config set user.name "${name}"
+    git config set user.email "${email}"
+    git config set user.signingkey "${key}"
+    git config set --bool commit.gpgsign true
+    git config set --bool tag.gpgsign true
+    git config set --bool tag.forcesignannotated true
+    if [ "${is_ssh_key}" = 'true' ]; then
+      git config set gpg.format ssh
+      if [ -n "${allowed_signers_file+x}" ]; then
+        git config set gpg.ssh.allowedSignersFile "${allowed_signers_file}"
+      else
+        git config unset gpg.ssh.allowedSignersFile
+      fi
+    else
+      git config set gpg.format openpgp
+      git config unset gpg.ssh.allowedSignersFile
+    fi
+    echo "$(git config get user.name) <$(git config get user.email)> $(git config get user.signingkey) - $PWD"
+  fi
+}
 
 cd "${base_dir}"
 
 if [ -n "${key}" ]; then
-  for dir in ./*/; do
-    if [ "${dir}" = './*/' ]; then
-      break
-    fi
-    cd "${dir}"
-    if [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = 'true' ]; then
-      # https://git-scm.com/docs/git-config#_variables
-      git config set user.name "${name}"
-      git config set user.email "${email}"
-      git config set user.signingkey "${key}"
-      git config set --bool commit.gpgsign true
-      git config set --bool tag.gpgsign true
-      git config set --bool tag.forcesignannotated true
-      if [ "${is_ssh_key}" = 'true' ]; then
-        git config set gpg.format ssh
-      else
-        git config set gpg.format openpgp
+  if [ "${recurse}" = 'true' ]; then
+    for dir in ./*/; do
+      if [ "${dir}" = './*/' ]; then
+        break
       fi
-      echo "$(git config get user.name) <$(git config get user.email)> $(git config get user.signingkey) - $PWD"
-    fi
-    cd ..
-  done
+      cd "${dir}"
+      set_identity_with_sign
+      cd ..
+    done
+  else
+    set_identity_with_sign
+  fi
 else
-  for dir in ./*/; do
-    if [ "${dir}" = './*/' ]; then
-      break
-    fi
-    cd "${dir}"
-    if [ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = 'true' ]; then
-      # https://git-scm.com/docs/git-config#_variables
-      git config set user.name "${name}"
-      git config set user.email "${email}"
-      git config set user.signingkey ''
-      git config set --bool commit.gpgsign false
-      git config set --bool tag.gpgsign false
-      git config set --bool tag.forcesignannotated false
-      git config set gpg.format openpgp
-      echo "$(git config get user.name) <$(git config get user.email)> - $PWD"
-    fi
-    cd ..
-  done
+  if [ "${recurse}" = 'true' ]; then
+    for dir in ./*/; do
+      if [ "${dir}" = './*/' ]; then
+        break
+      fi
+      cd "${dir}"
+      set_identity_no_sign
+      cd ..
+    done
+  else
+    set_identity_no_sign
+  fi
 fi
