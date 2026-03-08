a4f588db2344afaf38a815ec1d9fd99d83d71325
Sebastian Davids <sdavids@gmx.de>
2026-03-08 13:06:45 +0100

    be more resilient if .gitignore does not exist
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

--- scripts/git/git_delete_dsstore_files.sh
+++ scripts/git/git_delete_dsstore_files.sh
@@ -37,7 +37,7 @@ fi
 
     printf "\nThe repository at '%s' does not ignore '.DS_Store' files.\n\n" "$(realpath "${base_dir}")"
     printf "You should add '.DS_Store' to your global exclusion file:\n\n  git config get core.excludesfile\n\n"
-    printf "And to your project's exclusion file:\n\n  %s\n\n" "$(realpath "${base_dir}/.gitignore")"
+    printf "And to your project's exclusion file:\n\n  %s\n\n" "$(realpath "${base_dir}")/.gitignore"
     printf -- "---\n\n"
   fi
 
